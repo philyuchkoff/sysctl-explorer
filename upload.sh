ab4e8c5330ad59f305028fe512b022064b4ad351
home <user@MacBook-Pro-user.local>
2024-10-15 19:19:02 +0300

    Refactoring for mkdocs

--- upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bundle exec jekyll clean \
-&& bundle exec jekyll build \
-&& aws --profile roudier s3 sync --delete _site/ s3://sysctl-explorer-prd \
-&& bundler exec jekyll algolia \
-&& wget --recursive --directory-prefix='/tmp/' https://sysctl-explorer.net \
-&& rm --fr /tmp/sysctl-explorer.net/ \
